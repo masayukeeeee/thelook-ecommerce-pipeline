a8909396dcfd4288d4973491e516672e7b0569c7
masayukeeeee <masayukeeeee@gmail.com>
2026-05-01 17:08:42 +0900

    sqls: staging layer queries

---
+++ dbt/thelook/models/staging/stg_distribution_centers.sql
@@ -0,0 +1,14 @@
+with source as (
+    select * from {{ source('thelook_raw', 'distribution_centers') }}
+),
+
+cleaned as (
+    select
+        cast(id as int) as distribution_center_id,
+        cast(name as string) as name,
+        cast(latitude as float) as latitude,
+        cast(longitude as float) as longitude,
+    from source
+)
+
+select * from cleaned
