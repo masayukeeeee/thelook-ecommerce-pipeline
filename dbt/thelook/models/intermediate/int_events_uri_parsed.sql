07936c26fa7cc379c33145cefc0a5f4b9ab77dd5
masayukeeeee <masayukeeeee@gmail.com>
2026-05-07 15:42:07 +0900

    feat/intermediate layer

---
+++ dbt/thelook/models/intermediate/int_events_uri_parsed.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='ephemeral') }}
+
+with base as (
+	select * from {{ ref('stg_events') }}
+),
+
+parsed as (
+	select
+		*,
+		regexp_extract(uri, '^/product/(\\d+)', 1) as viewed_product_id,
+		regexp_extract(uri, '^/department/([^/]+)/category/([^/]+)/brand/([^/]+)', 1) as viewed_department_gender,
+		regexp_extract(uri, '^/department/([^/]+)/category/([^/]+)/brand/([^/]+)', 2) as viewed_category,
+		regexp_extract(uri, '^/department/([^/]+)/category/([^/]+)/brand/([^/]+)', 3) as viewed_brand
+from base
+)
+
+select * from parsed
